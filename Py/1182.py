06b64ec0a094f086ced0f9d2d7b6c626985c4f74
Jader Cerqueira <jader58@live.com>
2026-04-09 13:43:21 -0300

    +++

---
+++ Py/1182.py
@@ -0,0 +1,21 @@
+numero = int(input())
+letra = input()
+
+linha = []
+matriz = []
+
+soma = 0
+
+for i in range(12):
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha[:])
+    linha.clear()
+
+for i in range(12):
+    soma = soma + matriz[i][numero]
+
+if letra == 'S':
+    print(f"{(soma):.1f}")
+else:
+    print(f"{(soma / 12):.1f}")
