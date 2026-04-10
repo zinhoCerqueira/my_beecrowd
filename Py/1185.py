8f1e7f1d6d09fc2aff3d6a843d705b84938c1108
Jader Cerqueira <jader58@live.com>
2026-04-09 21:07:00 -0300

    +++

---
+++ Py/1185.py
@@ -0,0 +1,23 @@
+letra = input()
+
+linha = []
+matriz = []
+
+soma, cont = 0, 0
+
+for i in range(12):
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha[:])
+    linha.clear()
+
+for i in range(12):
+    for j in range(12):
+        if j + i < 11:
+            soma = soma + matriz[i][j]
+            cont = cont + 1
+
+if letra == 'S':
+    print(f"{(soma):.1f}")
+else:
+    print(f"{(soma / cont):.1f}")
