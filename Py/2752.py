97435c7a3ded10f1668846519f008bdd8dba9c83
Jader Cerqueira <jader58@live.com>
2026-06-05 09:46:36 -0300

    +

---
+++ Py/2752.py
@@ -0,0 +1,10 @@
+s = "AMO FAZER EXERCICIO NO URI"
+
+print(f"<{s}>")
+print(f"<{s:>30}>")
+print(f"<{s[:20]}>")
+print(f"<{s:<20}>")
+print(f"<{s:<30}>")
+print(f"<{s[:30]}>")
+print(f"<{s[:20]:>30}>")
+print(f"<{s[:20]:<30}>")
---
+++ Py/2753.py
@@ -0,0 +1,3 @@
+for i in range(97, 123):
+
+    print(i, "e", chr(i))
