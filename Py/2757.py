4d0e95be36099c5ff2d465a0a54154a282adfa68
Jader Cerqueira <jader58@live.com>
2026-06-06 11:54:47 -0300

    +

---
+++ Py/2757.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(f"A = {a}, B = {b}, C = {c}")
+print(f"A = {a:>10}, B = {b:>10}, C = {c:>10}")
+print(f"A = {a:010d}, B = {b:010d}, C = {c:010d}")
+print(f"A = {a:<10}, B = {b:<10}, C = {c:<10}")
