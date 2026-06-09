b903b7e38be2e66e661145cae42f9ad541ebc65b
Jader Cerqueira <jader58@live.com>
2026-06-09 08:55:34 -0300

    +

---
+++ Py/2764.py
@@ -0,0 +1,7 @@
+a = input()
+
+dd, mm, aa = a.split("/")
+
+print(f"{mm}/{dd}/{aa}")
+print(f"{aa}/{mm}/{dd}")
+print(f"{dd}-{mm}-{aa}")
---
+++ Py/2765.py
@@ -0,0 +1,6 @@
+a = input()
+
+frase = a.split(",")
+
+for parte in frase:
+    print(parte)
