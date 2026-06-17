9f85734005eb6c579b1b7dbdb33149cac842c918
Jader Cerqueira <jader58@live.com>
2026-06-17 08:45:49 -0300

    +

---
+++ Py/2791.py
@@ -0,0 +1,13 @@
+a, b, c, d = map(int, input().split())
+
+if a == 1:
+    print(1)
+
+elif b == 1:
+    print(2)
+
+elif c == 1:
+    print(3)
+
+elif d == 1:
+    print(4)
---
+++ Py/2802.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+n2 = n * n
+n3 = n2 * n
+n4 = n2 * n2
+
+x = (n4 - 6 * n3 + 23 * n2 - 18 * n + 24) // 24
+
+print(x)
