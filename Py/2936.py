ec18e608e07ba7860fd4d1c5af5e0ef5f326d4c4
Jader Cerqueira <jader58@live.com>
2026-06-24 18:38:38 -0300

    +

---
+++ Py/2936.py
@@ -0,0 +1,18 @@
+total = 0
+
+curupira = int(input())
+total = total + ( curupira * 300 )
+
+boitata = int(input())
+total = total + ( boitata * 1500 )
+
+boto = int(input())
+total = total + ( boto * 600 )
+
+mapinguari = int(input())
+total = total + ( mapinguari * 1000 )
+
+iara = int(input())
+total = total + ( iara * 150 )
+
+print(total + 225)
