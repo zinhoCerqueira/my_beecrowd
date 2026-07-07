eb886809a44b0a1572f5fdca7f65c5a47827fc61
Jader Cerqueira <jader58@live.com>
2026-07-07 10:11:08 -0300

    +

--- Py/3076.py
+++ Py/3076.py
@@ -1,17 +1,12 @@
 while True:
     try:
-
-        hora, minuto = map(int, input().split())
-
-        if ano <= 100:
-            print(1)
-        
-        else:
-
-            ano = ano + 1
-            resp = int( ano / 100 )
-
-            print(resp)
-
+        ano = int(input())
     except EOFError:
         break
+
+    if ano % 100 == 0:
+        seculo = ano // 100
+    else:
+        seculo = ano // 100 + 1
+
+    print(seculo)
